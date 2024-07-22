8fe6311a143626ddce51e48cc410b72dbe1e9274
Donat Kusari <donatkusari18@gmail.com>
2024-07-22 13:41:41 +0200

    Used variables to hide sensitive information on project and image repos

--- Terraform/Artifact_Registry/main.tf
+++ Terraform/Artifact_Registry/main.tf
@@ -1,18 +1,18 @@
 provider "google" {
-  project = "e-commerce-life"
-  region = "europe-west3"
+  project = var.project
+  region = var.region
 }
 
 resource "google_artifact_registry_repository" "frontend-repo" {
-  location = "europe-west3"
-  repository_id = "frontend-repo"
+  location = var.region
+  repository_id = var.frontend_repo_name
   description = "Repository for Frontend docker images of E-Commerce project"
   format = "DOCKER"
 }
 
 resource "google_artifact_registry_repository" "backend-repo" {
-  location = "europe-west3"
-  repository_id = "backend-repo"
+  location = var.region
+  repository_id = var.backend_repo_name
   description = "Repository for Backend docker images of E-Commerce project"
   format = "DOCKER"
 }
---
+++ Terraform/Artifact_Registry/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  description = "The project ID to deploy to"
+  type = string
+}
+
+variable "region" {
+  description = "The region to deploy to"
+  type = string
+}
+
+variable "frontend_repo_name" {
+  description = "The name of the image repository for the Frontend"
+  type = string
+}
+
+variable "backend_repo_name" {
+  description = "The name of the image repository for the Frontend"
+  type = string
+}
