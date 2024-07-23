3a793139cfba32ef6158168fd376c045927ed0ee
Donat Kusari <donatkusari18@gmail.com>
2024-07-23 13:00:48 +0200

    Added config files for creating SQL Server database on Google Cloud SQL

---
+++ Terraform/Cloud_SQL/main.tf
@@ -0,0 +1,48 @@
+provider "google" {
+  project = var.project
+  region = var.region
+}
+
+resource "google_sql_database_instance" "sql_server_instance" {
+  name = var.instance_name
+  region = var.region
+  deletion_protection = false
+
+  database_version = "SQLSERVER_2019_STANDARD"
+  
+  settings {
+    tier = "db-custom-2-8192"
+    availability_type = "REGIONAL"
+    
+    backup_configuration {
+      enabled = true
+      start_time = "02:00"
+      location = var.region
+    }
+  }
+  
+  root_password = var.root_password
+}
+
+resource "google_sql_user" "sql_server_user" {
+  name = var.database_user_name
+  password = var.database_user_password
+  instance = google_sql_database_instance.sql_server_instance.name
+}
+
+resource "google_sql_database" "sql_server_database" {
+  name = var.database_name
+  instance = google_sql_database_instance.sql_server_instance.name
+}
+
+resource "google_compute_firewall" "allow-sql" {
+  name = "allow-sql"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["1433"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
---
+++ Terraform/Cloud_SQL/variables.tf
@@ -0,0 +1,34 @@
+variable "project" {
+  description = "The project ID to deploy to"
+  type = string
+}
+
+variable "region" {
+  description = "The region to deploy to"
+  type = string
+}
+
+variable "root_password" {
+  description = "The password for the root"
+  type = string
+}
+
+variable "instance_name" {
+  description = "The name of the database instance"
+  type = string
+}
+
+variable "database_user_name" {
+  description = "The name of the database user"
+  type = string
+}
+
+variable "database_user_password" {
+  description = "The password of the database user"
+  type = string
+}
+
+variable "database_name" {
+  description = "The name of the database"
+  type = string
+}
